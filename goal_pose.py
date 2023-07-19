717ecdef6f4327b208c62b425ef224bb00fa5d18
Jack Dewan <jack.dewan4@gmail.com>
2023-07-18 20:07:36 -0400

    experiment 1 attempt 1

--- goal_pose.py
+++ goal_pose.py
@@ -26,18 +26,50 @@ rospy.init_node('goal_pose')
 navclient = actionlib.SimpleActionClient('move_base',MoveBaseAction)
 navclient.wait_for_server()
 
-# Example of navigation goal
+# first intermediate pose
 goal = MoveBaseGoal()
 goal.target_pose.header.frame_id = "map"
 goal.target_pose.header.stamp = rospy.Time.now()
 
-goal.target_pose.pose.position.x = -0.6166
-goal.target_pose.pose.position.y = 0.5498
+goal.target_pose.pose.position.x = -1.95
+goal.target_pose.pose.position.y = -0.115
 goal.target_pose.pose.position.z = 0.0
 goal.target_pose.pose.orientation.x = 0.0
 goal.target_pose.pose.orientation.y = 0.0
-goal.target_pose.pose.orientation.z = 0.8698
-goal.target_pose.pose.orientation.w = 0.4933
+goal.target_pose.pose.orientation.z = 0.6857
+goal.target_pose.pose.orientation.w = 0.7278
+
+navclient.send_goal(goal, done_cb, active_cb, feedback_cb)
+finished = navclient.wait_for_result()
+
+navclient.wait_for_server()
+#second intermediate pose
+goal.target_pose.header.frame_id = "map"
+goal.target_pose.header.stamp = rospy.Time.now()
+
+goal.target_pose.pose.position.x = -1.8126
+goal.target_pose.pose.position.y = -0.9316
+goal.target_pose.pose.position.z = 0.0
+goal.target_pose.pose.orientation.x = 0.0
+goal.target_pose.pose.orientation.y = 0.0
+goal.target_pose.pose.orientation.z = 0.760
+goal.target_pose.pose.orientation.w = 0.649
+
+# third and final goal pose
+navclient.wait_for_server()
+goal.target_pose.header.frame_id = "map"
+goal.target_pose.header.stamp = rospy.Time.now()
+
+goal.target_pose.pose.position.x = -0.855
+goal.target_pose.pose.position.y = -1.736
+goal.target_pose.pose.position.z = 0.0
+goal.target_pose.pose.orientation.x = 0.0
+goal.target_pose.pose.orientation.y = 0.0
+goal.target_pose.pose.orientation.z = -0.999
+goal.target_pose.pose.orientation.w = 0.0377
+
+navclient.send_goal(goal, done_cb, active_cb, feedback_cb)
+finished = navclient.wait_for_result()
 
 navclient.send_goal(goal, done_cb, active_cb, feedback_cb)
 finished = navclient.wait_for_result()
